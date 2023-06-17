afbd9b33a1c66dbdec1f9ea9c254bb0b05f9077e
thanhdinhit <thanhdinhit1280@gmail.com>
2023-06-17 21:18:34 +0700

    update tool for type office file

--- main.bat
+++ main.bat
@@ -7,6 +7,7 @@ set folder_path_sub="sample"
 set folder_path_output="output"
 
 echo Type: [0: file *.py] , [1: file *.bat], [2: file *.txt]
+echo Type: [3: file *.docx] , [4: file *.xlsx], [5: file *.pptx]
 set /p "type=File Type: "
 set /p "name=File Name: "
 
@@ -22,6 +23,15 @@ IF %type%==0 (
 ) ELSE IF %type%==2 (
     echo Create text file
     echo hello>%folder_path%\%folder_path_output%\%name%.txt
+) ELSE IF %type%==3 (
+    echo Create word file
+	copy NUL "%folder_path%\%folder_path_output%\%name%.docx"
+) ELSE IF %type%==4 (
+    echo Create excel file
+	copy NUL "%folder_path%\%folder_path_output%\%name%.xlsx"
+) ELSE IF %type%==5 (
+    echo Create powerpoint file
+	copy NUL "%folder_path%\%folder_path_output%\%name%.pptx"
 ) ELSE (
     echo Number in-correct
     set /p "type=type_number : "
